b11cf853213c266349246cb587cd35941b8291dc
Santiago Castillo Quiroga <santiago.castilloqu@ecci.edu.co>
2026-08-27 19:12:03 -0500

    Update resistance calculation using Callendar-Van Dusen

--- A_Sin_interaccion/A4_Resistencia_RTD.py
+++ A_Sin_interaccion/A4_Resistencia_RTD.py
@@ -1,10 +1,18 @@
-#Para este ejercicio usaremos la ecuacion Callendar-Van Dusen (R = R0 * (1 + A*T + B*T**2))
-#Donde R0 es la resistencia a 0°C, A y B son constantes de la ecuacion y T es la temperatura en grados Celsius.
+# Para este ejercicio usaremos la ecuacion Callendar-Van Dusen
+# R = R0 * (1 + A*T + B*T**2 + C*(T - 100)*T**3)
 import numpy as np
-R0 = 100 #Resistencia a 0°C
-A = 3.9083e-3 #Constante A
-B = -5.775e-7 #Constante B
-T = float(input("Ingresar temperatura en °C: ")) #Temperatura en 
-#Calculo de la resistencia usando la ecuacion Callendar-Van Dusen
-R = R0 * (1 + A*T + B*T**2)
+
+R0 = 100
+A = 3.9083e-3
+B = -5.775e-7
+C = -4.183e-12
+
+T = float(input("Ingresar temperatura en °C: "))
+
+# Calculo de la resistencia
+if T < 0:
+    R = R0 * (1 + A*T + B*T**2 + C*(T - 100)*T**3)
+else:
+    R = R0 * (1 + A*T + B*T**2)
+
 print("La resistencia es:", R, "Ω")
