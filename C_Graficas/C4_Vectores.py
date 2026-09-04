85c4b932391a1e3fda1dd8119682c9aa936df1dc
Santiago Castillo Quiroga <santiago.castilloqu@ecci.edu.co>
2026-09-03 19:04:41 -0500

    Implement dynamic limits for 3D vector plot
    Added dynamic limits to the 3D vector plot based on the maximum absolute value of the vector components.

--- C_Graficas/C4_Vectores.py
+++ C_Graficas/C4_Vectores.py
@@ -1,21 +1,46 @@
 import matplotlib
+
 matplotlib.use("TkAgg")
+
 import matplotlib.pyplot as plt
 import numpy as np
-valores= input("Escriba los valores del vector por separado [x,y,z]:")
-numeros=[int(x) for x in valores.split()]
-vector= np.array(numeros)
-x= vector[0]
-y= vector[1]
-z= vector[2]
-figura= plt.figure()
-eje= figura.add_subplot(111, projection="3d")
-eje.quiver(0,0,0,x,y,z)
+
+valores = input("Escriba los valores del vector por separado: ")
+
+numeros = [int(x) for x in valores.split()]
+
+vector = np.array(numeros)
+
+x = vector[0]
+y = vector[1]
+z = vector[2]
+
+mayor = max(abs(x), abs(y), abs(z))
+
+limite = mayor + 2
+
+figura = plt.figure()
+
+eje = figura.add_subplot(111, projection="3d")
+
+eje.quiver(
+    0, 0, 0,
+    x, y, z,
+    color="blue",
+    arrow_length_ratio=0.1
+)
+
 eje.set_xlabel("Eje X")
 eje.set_ylabel("Eje Y")
 eje.set_zlabel("Eje Z")
+
 eje.set_title("Ejercicio Vector")
-eje.set_xticks(np.arange(-6, 7, 1))
-eje.set_yticks(np.arange(-6, 7, 1))
-eje.set_zticks(np.arange(-6, 7, 1))
+
+
+# Límites dinámicos
+eje.set_xlim(-limite, limite)
+eje.set_ylim(-limite, limite)
+eje.set_zlim(-limite, limite)
+
+
 plt.show()
